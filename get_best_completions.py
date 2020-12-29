9a9583c70d3e674205e72a48ff0badf214ba69ed
Shoshi <shoshirizel20@gmail.com>
2020-12-29 12:38:05 +0200

    Add files via upload

---
+++ get_best_completions.py
@@ -0,0 +1,55 @@
+import json
+import string
+import re
+
+from pathlib import Path
+
+from auto_complete_data import AutoCompleteData
+
+DATA_FOLDER = Path("data")
+MAX_CHARACTERS = 5
+
+
+def fix_sentence(sentence):
+    s = sentence[:]
+    s.translate(str.maketrans('', '', string.punctuation))
+    s = re.sub(' +', ' ', s)
+
+    return s.lower()
+
+
+class Complete:
+    def __init__(self):
+        self.data_dict = {}
+        self.search_dict = {}
+        self.open_files()
+
+    def open_files(self):
+        with (DATA_FOLDER / "data.json").open() as data_file:
+            self.data_dict = json.load(data_file)
+
+        with (DATA_FOLDER / "search_file.json").open() as search_file:
+            self.search_dict = json.load(search_file)
+
+    def create_auto_complete(self, completion_list, search_input):
+        object_completion_list = []
+
+        for completion in completion_list:
+            object_completion_list.append(AutoCompleteData(
+                self.data_dict[str(completion["id"])],
+                search_input,
+                completion["score"],
+                completion["offset"])
+            )
+
+        return object_completion_list
+
+    def get_best_completions(self, search_input):
+        fix_input = fix_sentence(search_input)[:MAX_CHARACTERS]
+
+        if not self.search_dict.get(fix_input):
+            return []
+
+        return self.create_auto_complete(
+            self.search_dict[fix_sentence(fix_input)],
+            search_input)
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from search import get_best_completions, open_files
+import search
 
 
 def input_fun():
     print("Loading the files and and preparing the system...")
-    open_files()
+    complete = search.Complete()
     search_input = 0
     while search_input != "-1":
         print("The system is ready. Enter your text:")
         search_input = input()
-        suggestion_list = get_best_completions(search_input)
+        suggestion_list = complete.get_best_completions(search_input)
         output_fun(suggestion_list)
         print(search_input)
 
@@ -18,7 +18,7 @@ def output_fun(suggestions_list):
     for index in range(len(suggestions_list)):
         print(
             f"{index + 1}. {suggestions_list[index].completed_sentence} \n"
-            f"score: {suggestions_list[index].score} \n "
+            f"  score: {suggestions_list[index].score} \n "
             f" offset: {suggestions_list[index].offset * -1}")
 
 
