4f40fe451c5e7282d5ebbf8029d2e578fc9015ad
Shoshi <sh0534157975@gmail.com>
2020-12-31 10:52:51 +0200

    Update get_best_completions.py

--- get_best_completions.py
+++ get_best_completions.py
@@ -3,6 +3,7 @@ import string
 import re
 
 from pathlib import Path
+from typing import List
 
 from auto_complete_data import AutoCompleteData
 
@@ -10,7 +11,7 @@ DATA_FOLDER = Path("data")
 MAX_CHARACTERS = 5
 
 
-def fix_sentence(sentence):
+def fix_sentence(sentence: str) -> str:
     s = sentence[:]
     s.translate(str.maketrans('', '', string.punctuation))
     s = re.sub(' +', ' ', s)
@@ -24,7 +25,7 @@ class Complete:
         with (DATA_FOLDER / "search_file.json").open() as search_file:
             self.search_dict = json.load(search_file)
 
-    def create_auto_complete(self, completion_list, search_input):
+    def create_auto_complete(self, completion_list: List[dict], search_input: str) -> List[AutoCompleteData]:
         object_completion_list = []
         for completion in completion_list:
             object_completion_list.append(AutoCompleteData(
@@ -35,7 +36,7 @@ class Complete:
             )
         return object_completion_list
 
-    def get_best_completions(self, search_input):
+    def get_best_completions(self, search_input: str) -> List[AutoCompleteData]:
         fix_input = fix_sentence(search_input)[:MAX_CHARACTERS]
         if not self.search_dict.get(fix_input):
             return []
